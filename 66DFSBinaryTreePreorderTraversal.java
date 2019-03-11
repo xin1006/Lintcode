0cedfb9c80ce4722675f1b315bf541bc1d329d0f
xin1006 <xxh160030@gmail.com>
2019-03-11 18:37:38 -0500

    Create 66DFSBinaryTreePreorderTraversal.java

---
+++ 66DFSBinaryTreePreorderTraversal.java
@@ -0,0 +1,39 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Preorder in ArrayList which contains node values.
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        // write your code here
+        List<Integer> res = new ArrayList();
+        Stack<TreeNode> stack = new Stack();
+        if (root == null) {
+            return res;
+        }
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return res;
+    }
+}
